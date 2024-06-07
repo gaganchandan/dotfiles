3d4918f86fd066915ce465cdd21c4edfe8298d32
gaganchandan <gaganchandan29@gmail.com>
2024-06-07 11:17:24 +0530

    Updates

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-    cmd = {'/usr/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    cmd = {'/usr/bin/jdtls',},
+    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
